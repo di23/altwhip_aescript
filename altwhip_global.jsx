17250625ec2411b6645b783c8a48620fd4a419d8
YH <di23@users.noreply.github.com>
2019-01-20 02:12:07 +0300

    Done global path file

---
+++ altwhip_global.jsx
@@ -0,0 +1,92 @@
+/*
+	altwhip_global
+	Author: Yahor Hayeuski
+	version: 1.00
+	Script for ft-Toolbar
+
+	Copy to clipboard expression path of the property. Path is global.
+*/
+( function (){
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+function find_universal_property_name( prop ){
+
+	if ( prop.parentProperty.propertyType === PropertyType.INDEXED_GROUP ){
+		return prop.name;
+	} else {
+		return prop.matchName;
+	}
+}
+
+// ---------------------------------
+// Checks
+// ---------------------------------
+
+// Check for selected comp
+var curcomp = app.project.activeItem;
+if (( curcomp == null ) || !( curcomp instanceof CompItem )){
+
+	alert( "There is no active comp." );
+	return; // Exit
+}
+
+// Check if only one layer selected
+if ( curcomp.selectedLayers.length != 1 ){
+
+	alert( "More than one layer selected? No selected properties?" );
+	return; // Exit
+}
+
+// Check if one property selected
+var selprop;
+
+var selected_props = curcomp.selectedProperties;
+if ( selected_props.length == 1 ){
+
+	selprop = selected_props[ 0 ];
+
+} else if ( selected_props.length == 2 ){ // Sometimes parent property is selected together with target property
+	if ( selected_props[ 0 ].matchName == selected_props[ 1 ].parentProperty.matchName ){
+
+		selprop = selected_props[ 1 ];
+
+	} else {
+		alert( "Select one property." );
+		return; // Exit
+	}
+} else {
+	alert( "Select one property." );
+	return; // Exit
+}
+
+// ---------------------------------
+// Property Path
+// ---------------------------------
+
+// Find all properties name
+var prop_hierarchy = [ find_universal_property_name( selprop )];
+
+for ( var i = 1; i < selprop.propertyDepth; i++ ){
+
+	prop_hierarchy.push( find_universal_property_name( selprop.propertyGroup( i )));
+}
+
+// Create path string
+var proppart_path = "";
+for ( var i = prop_hierarchy.length - 1; i >= 0; i-- ){
+
+	proppart_path += '("' + prop_hierarchy[ i ] + '")';
+}
+var path = 'comp("' + curcomp.name + '").layer("' + curcomp.selectedLayers[ 0 ].name + '")' + proppart_path;
+
+// ---------------------------------
+// Copy To Clipboard
+// ---------------------------------
+
+var cmd_string = 'cmd.exe /c cmd.exe /c "echo|set/p=' + path + '|clip"'; // NOTE: Windows only
+system.callSystem( cmd_string );
+
+})(); // end
