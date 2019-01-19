338991be88957fd256575669b2dc0fd7eed53a05
YH <di23@users.noreply.github.com>
2019-01-19 03:17:41 +0300

    find property index

--- altwhip.jsx
+++ altwhip.jsx
@@ -8,11 +8,16 @@ if (( curcomp == null ) || !( curcomp instanceof CompItem )){
 }
 
 // Check if one property selected
-if ( curcomp.selectedProperties.length != 1 ){
+if ( curcomp.selectedProperties.length != 1 ){ // FIXME пропорция выделяется не одна а вместе с эффектом
 	alert( "Select one property." );
 	return;
 }
 
-alert( "yes" );
+var selprop = curcomp.selectedProperties[ 0 ];
+
+// Find property path
+var propid = selprop.propertyIndex;
+
+alert( propid );
 
 })(); // end
