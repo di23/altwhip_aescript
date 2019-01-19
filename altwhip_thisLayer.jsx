f750b4deb0808b14001a43aa9bcabedf0bb733b2
YH <di23@users.noreply.github.com>
2019-01-20 01:14:38 +0300

    refactor complite

--- altwhip_thisLayer.jsx
+++ altwhip_thisLayer.jsx
@@ -1,59 +1,85 @@
+/*
+	altwhip_thisLayer
+	Author: Yahor Hayeuski
+	version: 1.00
+	Script for ft-Toolbar
+
+	Copy to clipboard expression path of the property. Path is relevant for "thisLayer".
+*/
 ( function (){
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+function find_universal_property_name( prop ){
+
+	if ( prop.parentProperty.propertyType === PropertyType.INDEXED_GROUP ){
+		return prop.name;
+	} else {
+		return prop.matchName;
+	}
+}
+
+// ---------------------------------
+// Checks
+// ---------------------------------
 
 // Check for selected comp
 var curcomp = app.project.activeItem;
 if (( curcomp == null ) || !( curcomp instanceof CompItem )){
+
 	alert( "There is no active comp." );
-	return;
+	return; // Exit
 }
 
 // Check if one property selected
 var selprop;
-if ( curcomp.selectedProperties.length == 1 ){
-	selprop = curcomp.selectedProperties[ 0 ];
 
-} else if ( curcomp.selectedProperties.length == 2 ){
-	if ( curcomp.selectedProperties[ 0 ].matchName == curcomp.selectedProperties[ 1 ].parentProperty.matchName ){
-		selprop = curcomp.selectedProperties[ 1 ];
+var selected_props = curcomp.selectedProperties;
+if ( selected_props.length == 1 ){
+
+	selprop = selected_props[ 0 ];
+
+} else if ( selected_props.length == 2 ){ // Sometimes parent property is selected together with target property
+	if ( selected_props[ 0 ].matchName == selected_props[ 1 ].parentProperty.matchName ){
+
+		selprop = selected_props[ 1 ];
+
 	} else {
 		alert( "Select one property." );
-		return;
+		return; // Exit
 	}
 } else {
 	alert( "Select one property." );
-	return;
+	return; // Exit
 }
 
-// Find property path
-var propid;
-if ( selprop.parentProperty.propertyType === PropertyType.INDEXED_GROUP ){
-	propid = selprop.name;
-} else {
-	propid = selprop.matchName;
-}
+// ---------------------------------
+// Property Path
+// ---------------------------------
 
-var prop_hierarchy = [ propid ];
-for ( var i = 1; i < selprop.propertyDepth - 1; i++ ){
-	var curname;
-	if ( selprop.propertyGroup( i + 1 ).propertyType === PropertyType.INDEXED_GROUP ){
-		curname = selprop.propertyGroup( i ).name;
-	} else {
-		curname = selprop.propertyGroup( i ).matchName;
-	}
-	prop_hierarchy.push( curname );
+// Find all properties name
+var prop_hierarchy = [ find_universal_property_name( selprop )];
+
+for ( var i = 1; i < selprop.propertyDepth; i++ ){
+
+	prop_hierarchy.push( find_universal_property_name( selprop.propertyGroup( i )));
 }
-prop_hierarchy.push( selprop.propertyGroup( selprop.propertyDepth - 1 ).matchName );
 
 // Create path string
 var proppart_path = "";
 for ( var i = prop_hierarchy.length - 1; i >= 0; i-- ){
+
 	proppart_path += '( "' + prop_hierarchy[ i ] + '" )';
 }
-
 var path = "thisLayer" + proppart_path;
 
-// Copy to clipboard
-var cmd_string = 'cmd.exe /c cmd.exe /c "echo|set/p=' + path + '|clip"'; 
+// ---------------------------------
+// Copy To Clipboard
+// ---------------------------------
+
+var cmd_string = 'cmd.exe /c cmd.exe /c "echo|set/p=' + path + '|clip"'; // NOTE: Windows only
 system.callSystem( cmd_string );
 
 })(); // end
